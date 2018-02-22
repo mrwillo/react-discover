347de9e4e325d14245f9f773344e60a3fc3c062e
mrwillo <tranduy86los@gmail.com>
2018-02-22 16:03:53 +0700

    add state in board and passed it to squre

--- tic-tac/src/index.js
+++ tic-tac/src/index.js
@@ -3,24 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        }
-    }
     render() {
         return (
-            <button className="square" onClick={()=>this.setState({value: 'X'})}>
-                {this.state.value}
+            <button className="square" onClick={()=>this.props.onClick()}>
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
     renderSquare(i) {
-        return <Square value={i}/>;
+        return (
+            <Square
+            value={this.state.squares[i]}
+            onClick={()=>this.handleClick(i)}
+            />
+        );
     }
 
     render() {
@@ -28,6 +38,7 @@ class Board extends React.Component {
 
         return (
             <div>
+                <div>{this.state.squares}</div>
                 <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
