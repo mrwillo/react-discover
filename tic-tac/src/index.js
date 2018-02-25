f7bac87efb09e8e7c3c9037f18e8f07cce876ef8
mrwillo <tranduy86los@gmail.com>
2018-02-25 11:56:29 +0700

    add xIsNext to check player

--- tic-tac/src/index.js
+++ tic-tac/src/index.js
@@ -16,13 +16,18 @@ class Board extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         }
     }
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
+
     }
     renderSquare(i) {
         return (
@@ -38,7 +43,6 @@ class Board extends React.Component {
 
         return (
             <div>
-                <div>{this.state.squares}</div>
                 <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
